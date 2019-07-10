65d01932581e6ac9c2a86e20d2f755b6c81b850f
Sergio Luis Beleño Díaz <38571812+Serbeld@users.noreply.github.com>
2019-07-10 13:03:21 -0500

    Tercer Código

---
+++ 3erCode.py
@@ -0,0 +1,35 @@
+# encoding: utf-8 
+
+# 3erCode.py
+
+'''Este código hace parte de una serie de códigos utilizados 
+para la práctica del lenguaje de programación Python'''
+
+# Programador Sergio Luis Beleño Díaz
+# 08.Julio.2019
+
+a = 2
+b = 2.4
+c = 4
+d = 3.5
+
+# SUMA
+print(a + b)
+
+# Resta
+print(c - a)
+
+# Multiplicación
+print(d * c)
+
+# Exponente
+print(c ** 2)
+
+# División
+print(c / a)
+
+# División entera
+print(int(c / a))
+
+# Modulo
+print( 7 % 3)
