85d5f91964830371f12bef53cef5ee5880d635d6
Sergio Luis Beleño Díaz <38571812+Serbeld@users.noreply.github.com>
2019-07-13 07:27:46 -0500

    Tuplas

---
+++ 5toCode.py
@@ -0,0 +1,15 @@
+# encoding: utf-8 
+
+# 5toCode.py
+
+'''Este código hace parte de una serie de códigos utilizados 
+para la práctica del lenguaje de programación Python'''
+
+# Programador Sergio Luis Beleño Díaz
+# 10.Julio.2019
+
+tupla = (True,'The', "earth is running", 'out',1)
+tipo = type(tupla)
+
+print(tipo)		# <class 'tuple'>
+print(tupla[2])	# earth is running
