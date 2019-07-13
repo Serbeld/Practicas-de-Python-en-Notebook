d566779cf01420f0bf101cefdec77ea7074a6b22
Sergio Luis Beleño Díaz <38571812+Serbeld@users.noreply.github.com>
2019-07-13 07:28:25 -0500

    Diccionarios

---
+++ 6xtoCode.py
@@ -0,0 +1,19 @@
+# encoding: utf-8 
+
+# 6xtoCode.py
+
+'''Este código hace parte de una serie de códigos utilizados 
+para la práctica del lenguaje de programación Python'''
+
+# Programador Sergio Luis Beleño Díaz
+# 11.Julio.2019
+
+d = {'Clave 1':[1,2,3,4],
+	 'Clave 2':'Beld',
+	 '1'      :'Sergio',
+	  2       :'Beleño',
+}
+
+print(d['Clave 2']) 	# Beld
+print(d['1'])			# Sergio
+print(d[2])				# Beleño
